8b50ea5992b5e6b0da0b6e19cb0c69e9e60908b0
Dtsoy <dmitriytsoy123@gmail.com>
2022-06-22 17:17:25 -0700

    getNewCharList

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -3,6 +3,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=f853eb8f8a61bb96ed6bcc367c35000b';
+    _baseOffset = 210
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,8 +15,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllChar = async () => {
-       const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=300&${this._apiKey}`);
+    getAllChar = async (offset = this._baseOffset) => {
+       const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
        return res.data.results.map(this._transformChar)
     }
 
